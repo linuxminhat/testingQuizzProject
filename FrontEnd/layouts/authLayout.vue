20daa360328409e4ec23a22369792f01584dc8ca
linuxminhat <dangnhatminhdhbk@gmail.com>
2024-07-31 18:57:19 +0700

    building layouts

---
+++ FrontEnd/layouts/authLayout.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="">
+        <Header class="fixed z-50 bg-white" />
+        <HomePage class="" />
+    </div>
+
+    <router-view />
+</template>
+
+<script>
+import Header from '../components/header/Header.vue';
+import HomePage from '../pages/HomePage.vue';
+export default {
+    components: { Header, HomePage }
+}
+</script>
+
+
+<style>
+
+</style>
